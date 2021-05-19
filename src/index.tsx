1ecc4bf0773e462a10df0936f230e1c337067df3
MikePreston17 <michael.n.preston@gmail.com>
2021-05-19 17:04:58 -0500

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import reportWebVitals from "./reportWebVitals";
+import { setupProject } from "./project-setup";
+import App from "./App";
+import "./index.css";
+
+// Renders the web-app/extension content to DOM.
+setupProject({
+  rootElement: (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ),
+  injectExtensionTo: ".root",
+  injectWebAppTo: "#root",
+});
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
